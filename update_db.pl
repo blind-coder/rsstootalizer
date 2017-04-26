3cfcef2ee663d73bc6e02e5cfb3939f86131f24c
Benjamin Schieder <benjamin@anderdonau.de>
2017-04-26 09:29:51 +0200

    fix script

--- update_db.pl
+++ update_db.pl
@@ -27,16 +27,14 @@ binmode STDOUT, ":utf8";
 my @migrations = glob ("migrations/*sql");
 foreach my $migration (@migrations){
 	my $sth = RSSTootalizer::DB->doSELECT("SELECT * FROM migrations WHERE name = ?", $migration);
-	if (!$sth){
+	if (scalar(@$sth) == 0){
 		print "Running migration $migration\n";
 		open (M, "<", $migration);
 		my $sql = "";
 		while (<M>){
 			chomp;
-			print "Read: $_\n";
 			$sql .= $_;
 			if ($sql =~ /;/){
-				print "Running: $sql\n";
 				RSSTootalizer::DB->doDELETE($sql); # Using doDELETE for lack of error handling...
 				$sql = "";
 			}
