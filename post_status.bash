fbb1d92462f7bf3306622fb67523ad697aae0847
Benjamin Schieder <benjamin@anderdonau.de>
2017-04-28 19:12:21 +0100

    remove stray -x parameter

--- post_status.bash
+++ post_status.bash
@@ -1,3 +1,3 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "${status}" | curl -sS -X POST --header "Authorization: Bearer ${1}" --header "Content-Type: application/json;charset=utf-8" "${2}/api/v1/statuses" -d@- 2>/dev/null
